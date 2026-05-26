9c77513335df36ceb51d01cc618ba97926289977
action <action@github.com>
2026-05-26 03:02:02 +0000

    Add decoded output file

--- output.js
+++ output.js
@@ -1,8 +1,8 @@
-//Tue May 26 2026 03:00:10 GMT+0000 (Coordinated Universal Time)
+//Tue May 26 2026 03:02:02 GMT+0000 (Coordinated Universal Time)
 //Base:<url id="cv1cref6o68qmpt26ol0" type="url" status="parsed" title="GitHub - echo094/decode-js: JS混淆代码的AST分析工具 AST analysis tool for obfuscated JS code" wc="2165">https://github.com/echo094/decode-js</url>
 //Modify:<url id="cv1cref6o68qmpt26olg" type="url" status="parsed" title="GitHub - smallfawn/decode_action: 世界上本来不存在加密，加密的人多了，也便成就了解密" wc="741">https://github.com/smallfawn/decode_action</url>
 /**
- * cron "7 7 7 7 7" SendSms.js
+ * cron "7 7 7 7 7" SendSms.js1
  * export Common_Phone="手机号"
  * export Common_Type=""//XiShiYan,WangChao,XinJiangBei,TongLu,ZSWY,SHPJ,YueCheng,DaChao,RongPanAn,LanJingLing,AiHaiYan,QingJiao,JingZhou
  * export OCR_SERVER="ocr服务"
